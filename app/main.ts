e2df91ad086a6ba05557aac1d535aea75dbd503e
aneel tripathee <aneeltripathy56@gmail.com>
2024-10-26 13:18:26 +0545

    test

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,16 @@
 // Examples:
 // - decodeBencode("5:hello") -> "hello"
 // - decodeBencode("10:hello12345") -> "hello12345"
-function decodeBencode(bencodedValue: string): string {
-  /* This function is used to decode a bencoded string
-    The bencoded string is a string that is prefixed by the length of the string
-    **/
-
-  // Check if the first character is a digit
+function decodeBencode(bencodedValue: string): string | number {
+  if (bencodedValue[0] === "i") {
+    let startIndex = bencodedValue.indexOf("i");
+    let endIndex = bencodedValue.indexOf("e");
+    if (endIndex === -1) {
+      throw new Error("Invalid encoded String");
+    } else {
+      return parseInt(bencodedValue.slice(startIndex, endIndex));
+    }
+  }
   if (!isNaN(parseInt(bencodedValue[0]))) {
     const firstColonIndex = bencodedValue.indexOf(":");
     if (firstColonIndex === -1) {
