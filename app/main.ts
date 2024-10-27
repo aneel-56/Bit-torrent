44520d6752245c119781dd1cbf2d79439681e448
aneel tripathee <aneeltripathy56@gmail.com>
2024-10-27 16:33:39 +0545

    test

--- app/main.ts
+++ app/main.ts
@@ -98,11 +98,10 @@ function bencode(data: Record<string, any> | string | number): Buffer {
       result += `${key.length}:${key}${bencode(value)}`;
     }
     return Buffer.from(result + "e");
-    } else if (typeof data === "string") {
-      return Buffer.from(`${data.length}:${data}`);
-    } else if (typeof data === "number") {
-      return Buffer.from(`i${data}e`);
-    }
+  } else if (typeof data === "string") {
+    return Buffer.from(`${data.length}:${data}`);
+  } else if (typeof data === "number") {
+    return Buffer.from(`i${data}e`);
   }
   throw new Error("Unsupported data type of bencoding");
 }
