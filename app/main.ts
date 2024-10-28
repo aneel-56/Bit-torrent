76860a7edbce2d6d5a18b968a9007ba332e3383b
aneel tripathee <aneeltripathy56@gmail.com>
2024-10-28 12:11:49 +0545

    pieces hashes

--- app/main.ts
+++ app/main.ts
@@ -154,7 +154,7 @@ if (args[2] === "decode") {
             .createHash("sha1")
             .update(pieceHashBuff)
             .digest("hex");
-          pieceHashes.push(pieceHash);
+          pieceHashes.push(pieceHash.toString("hex"));
         }
       } else {
         console.error("Invalid format for pieces");
