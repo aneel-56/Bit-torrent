c4fe0bd9546237453bf39487e8e4d2a6ce687e92
aneel tripathee <aneeltripathy56@gmail.com>
2024-10-29 12:14:20 +0545

    discover peers

--- app/main.ts
+++ app/main.ts
@@ -160,8 +160,8 @@ if (args[2] === "decode") {
       console.error("Failed to parse torrent data");
     }
     const trackerUrl = announce;
-    const randId = crypto.randomBytes(20);
-    const peerId = randId.toString("binary");
+    const randId = crypto.randomBytes(20).slice(0, 20);
+    const peerId = randId.toString("hex");
     console.log("peerId", peerId);
     const encodeInfoHash = encodeURIComponent(infoHash);
     const port = 6881;
