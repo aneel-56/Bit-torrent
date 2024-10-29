844b648339005b3e42bfea22a6115837dd2c6590
aneel tripathee <aneeltripathy56@gmail.com>
2024-10-29 13:53:48 +0545

    discover peers

--- app/main.ts
+++ app/main.ts
@@ -177,14 +177,14 @@ if (args[2] === "decode") {
         const decodedResponse = decodeBencode(
           response.data.toString("binary")
         ) as unknown as TrackerResponse;
-        const peers = Buffer.from(decodedResponse.peers);
+        const peers = Buffer.from(decodedResponse.peers).toString();
         // console.log("Peers:", peers);
         const peerList: string[] = [];
         for (let i = 0; i < peers.length; i += 6) {
           const ip = `${peers[i]}.${peers[i + 1]}.${peers[i + 2]}.${
             peers[i + 3]
           }`;
-          const port = peers[i + 4] * 256 + peers[i + 5];
+          const port = peers[i + 5];
           console.log(port);
           peerList.push(`${ip}:${port}`);
         }
