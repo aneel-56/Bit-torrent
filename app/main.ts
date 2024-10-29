481eaa85c0cfe6b27a8067cb52e9709e1c0db9dd
aneel tripathee <aneeltripathy56@gmail.com>
2024-10-29 12:09:33 +0545

    discover peers

--- app/main.ts
+++ app/main.ts
@@ -140,15 +140,15 @@ if (args[2] === "decode") {
     let pieces = info?.["pieces"];
     // console.log(Buffer.from(pieces).toString("hex"));
     if (typeof announce === "string" && typeof length === "number") {
-      console.log(`Tracker URL: ${announce}`);
-      console.log(`Length: ${length}`);
+      // console.log(`Tracker URL: ${announce}`);
+      // console.log(`Length: ${length}`);
       const bencodedInfo = bencode(info);
       var infoHash = crypto
         .createHash("sha1")
         .update(bencodedInfo)
         .digest("hex");
-      console.log("Info Hash:", infoHash);
-      console.log("Piece Length:", pieceLength);
+      // console.log("Info Hash:", infoHash);
+      // console.log("Piece Length:", pieceLength);
       let result: string[] = [];
       for (let pos = 0; pos < pieces.length; pos += 20) {
         result.push(
